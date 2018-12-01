baa8b627f8a24078a22360c3919328d8b24663f6
Allyn Alford <allynalford@gmail.com>
2018-11-30 20:24:13 -0500

    rearranged

---
+++ app/shared/parse/parse.go
@@ -0,0 +1,84 @@
+package parse
+
+import (
+	"app/model"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+//LoadBcpaFromDoc used to load Bcpa data from HTML
+func LoadBcpaFromDoc(doc *goquery.Document) model.Bcpa {
+
+	bcpa := model.Bcpa{}
+	var siteAddress, owner, mailingAddress, id, mileage, use, legal string
+
+	// use selector found with the browser inspector
+	siteAddress = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > span > a > b").Contents().Text()
+
+	//clean up the carriage return
+	re := regexp.MustCompile(`\r?\n`)
+	siteAddress = re.ReplaceAllString(siteAddress, " ")
+
+	//Set the Object
+	bcpa.Siteaddress = strings.TrimSpace(StripSpaces(siteAddress))
+
+	owner = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > span").Contents().Text()
+	//Set the Object
+	bcpa.Owner = strings.TrimSpace(owner)
+
+	mailingAddress = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2) > span").Contents().Text()
+
+	//Set the Object
+	bcpa.MailingAddress = strings.TrimSpace(mailingAddress)
+
+	id = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(2) > span").Contents().Text()
+
+	//Set the Object
+	bcpa.ID = strings.TrimSpace(strings.Replace(StripSpaces(id), " ", "", -1))
+
+	mileage = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > span").Contents().Text()
+
+	//Set the Object
+	bcpa.Milage = strings.TrimSpace(mileage)
+
+	use = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(2) > span").Contents().Text()
+
+	//Set the Object
+	bcpa.Use = strings.TrimSpace(StripSpaces(use))
+
+	legal = doc.Find("body > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(4) > tbody > tr > td:nth-child(2) > span").Contents().Text()
+
+	//Set the Object
+	bcpa.Legal = strings.TrimSpace(legal)
+
+	return bcpa
+}
+
+// marshalBcpa Convert BCPA	to string
+func marshalBcpa(bcpa model.Bcpa) string {
+	//user := &User{name:"Frank"}
+	b, err := json.Marshal(bcpa)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return "0"
+	}
+	fmt.Println(string(b))
+
+	return string(b)
+}
+
+//StripSpaces remove leading and trailing and extra gapped spaces
+func StripSpaces(o string) string {
+
+	releadclosewhtsp2 := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
+
+	reinsidewhtsp2 := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+
+	final := releadclosewhtsp2.ReplaceAllString(o, "")
+
+	return reinsidewhtsp2.ReplaceAllString(final, " ")
+}
